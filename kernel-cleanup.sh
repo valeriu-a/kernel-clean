654ae4631ed0bd2839477cd75a8f7e7fd1c5cb66
Valeriu Cuc <cuc.valeriu@gmail.com>
2017-10-26 11:24:16 +0300

    Fixed formatting and set apt to assume yes

--- kernel-cleanup.sh
+++ kernel-cleanup.sh
@@ -6,16 +6,16 @@ set -e
 KERNEL_VERS=$(uname -r)
 KERNEL_REM_LIST=$(dpkg --list 'linux-image*' | awk '{ if ($1=="ii") print $2}' | grep -v '$KERNEL_VERS')
 
-echo -e "The kernel version you are running is $KERNEL_VERS\n."
+echo -e "The kernel version you are running is $KERNEL_VERS.\n"
 echo -e "You can remove the following kernel packages:"
 echo -e "$KERNEL_REM_LIST\n"
 
 read -p "Do you want to continue? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
   then
-    sudo apt-get purge $KERNEL_REM_LIST
-    sudo apt-get autoremove
+    sudo apt-get --yes purge $KERNEL_REM_LIST
+    sudo apt-get --yes autoremove
     sudo update-grub
-    sudo apt-get clean
+    sudo apt-get --yes clean
     sudo apt-get update
 fi

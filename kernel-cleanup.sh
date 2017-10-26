bed1a7d68f1a85ca49312173f3e68ce10c007a2c
Valeriu Cuc <cuc.valeriu@gmail.com>
2017-10-26 11:12:04 +0300

    Improved dpkg query

--- kernel-cleanup.sh
+++ kernel-cleanup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo -k
+set -e
 
 KERNEL_VERS=$(uname -r)
-KERNEL_REM_LIST=$(dpkg-query -W -f='${binary:Package}\n' 'linux-image*' | tail -n +2 | grep -v $KERNEL_VERS)
+KERNEL_REM_LIST=$(dpkg --list 'linux-image*' | awk '{ if ($1=="ii") print $2}' | grep -v ''$KERNEL_VERS')
 
 echo -e "The kernel version you are running is $KERNEL_VERS\n."
 echo -e "You can remove the following kernel packages:"
@@ -12,7 +13,7 @@ echo -e "$KERNEL_REM_LIST\n"
 read -p "Do you want to continue? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
   then
-    sudo apt-get remove $KERNEL_REM_LIST
+    sudo apt-get purge $KERNEL_REM_LIST
     sudo apt-get autoremove
     sudo update-grub
     sudo apt-get clean
